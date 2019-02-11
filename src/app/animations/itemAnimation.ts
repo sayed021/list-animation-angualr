756a94c9f48f604f6d6ac281483ae936f74952d2
sayed021 <mdabusayedkpi@gmail.com>
2019-02-11 13:55:09 +0600

    apply animation

--- src/app/animations/itemAnimation.ts
+++ src/app/animations/itemAnimation.ts
@@ -1,11 +1,38 @@
 import {
-    stagger,
     state,
     animate,
     transition,
     query,
     group,
-    style
+    style,
+    trigger,
+    stagger
 } from "@angular/animations";
 
+export const fadeInOutAnimation =
 
+trigger('fadeInOutAnimation', [
+    transition('* => *', [ 
+
+        query(':enter', [
+            style({
+                opacity: 0,
+                transform: 'translateX(-100%) scale(1.2)'
+            }),
+            stagger('100ms', [
+            animate('0.5s', style({ opacity: 1 , transform: 'translateX(0) scale(1)'}))
+            ])
+        ], { optional: true }),
+
+        query(':leave', [
+            style({
+                opacity:1,
+                transform: 'translateX(0)'
+            }),
+            stagger('100ms', [
+                animate('0.5s', style({ opacity: 0, transform: 'translateX(-100%) scale(1)' }))
+            ])
+        ], { optional: true })
+        
+    ])
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { fadeInOutAnimation } from './animations/itemAnimation';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[fadeInOutAnimation]
 })
+
 export class AppComponent {
-  title = 'listAnimation';
-  items=['item','item','item','item','item','item','item'];
+  title = 'fadeInOutAnimation';
+  items=['item 1','item 2','item 3','item 4','item 5','item 6','item 7'];
+
+
+  deleteItem(i) {
+    this.items.splice(i,1);
+  }
+  
 }
